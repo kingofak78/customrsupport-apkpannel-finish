be0bf0cca73932f20ca72f9181ec8fdea7efc542
kingofak78 <gotakale952@gmail.com>
2025-03-24 02:38:05 -0700

    Delete routes directory

--- routes/StatusRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { updateDeviceStatus } = require('../controllers/StatusController');
-
-router.post('/updatee', updateDeviceStatus);
-
-module.exports = router;
--- routes/detail.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const detailController = require('../controllers/detailController');
-
-// Route to fetch user details
-router.get('/detail/:uniqueid', detailController.getUserDetails);
-
-module.exports = router;
